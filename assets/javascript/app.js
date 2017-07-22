90777448eec542bc7a517571e787ee3b5801cd93
Manish Garg <manishgargpro@gmail.com>
2017-07-22 17:23:07 -0400

    it works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -48,71 +48,77 @@ var game = {
   wrong: 0,
 
   //something that populates the html with the question
-  askQuestions: function(e){
-  	clearTimeout(timer);
-    var questionInPlay = this.questions[e].questionText;
+  askQuestions: function(){
+    clearTimeout(timer);
+    var questionInPlay = this.questions[this.x].questionText;
     $("#question-text").html(questionInPlay);
-    this.generateAnswers(e);
+    this.generateAnswers(this.x);
     time = 10;
-		$("#timer-count").html(time);
-    createTimer();
-		if(time <= 0){
-			this.wrongAnswer();
-		}
+    $("#timer-count").html(time);
+    createTimer1();
   },
 
   //something that populates the html with the answers
-  generateAnswers: function(e){
-    var answerArray = this.questions[e].answers;
+  generateAnswers: function(j){
+    var answerArray = this.questions[j].answers;
     for (i = 0; i < answerArray.length; i++){
       $("#answer"+i).html(answerArray[i]);
     }
     $(".answer").css("background-color", "inherit");
+    $(".answer").hover(
+    		function(){$(this).css("background-color", "#acf9ff")},
+    		function(){$(this).css("background-color", "inherit")}
+    	);
   },
 
   //ask the first question and show the timer
   initializeGame: function(){
-  	this.askQuestions(0);
-  	$("#timer").css("display", "block");
+  	this.correct = 0;
+  	this.wrong = 0;
+  	this.x = 0
+    this.askQuestions();
+    $("#timer").css("display", "block");
   },
 
   //show the right answer regardless of what the user chooses
   showCorrectAnswer: function(){
-		$(".answer:contains("+this.questions[this.x].rightAnswer+")").css("background-color", "#6fff43");
-		clearTimeout(timer);
-		time = 3;
-		$("#timer-count").html(time);
-		createTimer();
-		if(time <= 0){
-			this.askQuestions(this.x);
-		}
+    $(".answer:contains("+this.questions[this.x].rightAnswer+")").css("background-color", "#6fff43");
+    clearTimeout(timer);
+    time = 1;
+    $("#timer-count").html(time);
+    //it took a while to figure out where to place this counter
+    this.xCounter();
+    createTimer2();
   },
 
   //define what happens when they choose the correct answer
   correctAnswer: function(){
-  	console.log("correct");
-  	$("#question-text").html("Correct!");
-  	this.correct++;
-	  this.xCounter();
-		this.showCorrectAnswer();
+    console.log("correct");
+    $("#question-text").html("Correct!");
+    this.correct++;
+    this.showCorrectAnswer();
   },
 
   //define what happens when they choose the wrong answer
   wrongAnswer: function(){
-		console.log("wrong");
-  	$("#question-text").html("Wrong!");
-  	this.wrong++;
-	  this.xCounter();
-		this.showCorrectAnswer();
+    console.log("wrong");
+    $("#question-text").html("Wrong!");
+    this.wrong++;
+    this.showCorrectAnswer();
+  },
+
+  endGame: function(){
+  	clearTimeout(timer);
+  	$("#question-text").html("Game Over");
+  	$("#answer0").html("Correct: " + this.correct);
+  	$("#answer1").html("Wrong or didn't answer: " + this.wrong);
+  	$("#answer2").html("");
+  	$("#answer3").html("");
   },
 
   //something that increments the index of whichever question is in play
   xCounter: function(){
-    if(this.x == this.questions.length - 1){
-      this.x = 0;
-    } else{
-      this.x++;
-    }
+    this.x++;
   }
 
 }
@@ -124,21 +130,46 @@ $("#question-text").click(function(){
 
 //what happens when you click any answer
 $(".answer").click(function(){
-	if($(this).html() == game.questions[game.x].rightAnswer){
-		game.correctAnswer();
-	} else {
-		game.wrongAnswer();
-	}
+  if($(this).html() == game.questions[game.x].rightAnswer){
+    game.correctAnswer();
+  } else {
+    game.wrongAnswer();
+  }
 });
 
+
+//call time variable
 var time;
 
+//call variable for settimeout function so that we can clear it later
 var timer;
 
-function createTimer(){
-	timer = setTimeout(function(){
-		time--;
-		$("#timer-count").html(time);
-		createTimer();
-	}, 1000);
+//I ended up having to create two different timer functions
+
+//this first on is specifically just for counting down while the question is waiting to be answered
+function createTimer1(){
+  timer = setTimeout(function(){
+    time--;
+    $("#timer-count").html(time);
+    createTimer1();
+    if(time <= 0){
+      game.wrongAnswer();
+    }
+  }, 1000);
+}
+
+//this second one is specifically for when an answer is either chosen or the first timer runs out
+function createTimer2(){
+  timer = setTimeout(function(){
+    time--;
+    $("#timer-count").html(time);
+    createTimer2();
+    if(time <= 0){
+    	if(game.x == game.questions.length){
+    		game.endGame();
+    	} else{
+    		game.askQuestions(game.x);
+    	}
+    }
+  }, 1000);
 }
